048f5d588b375962384f43ee2fc55e00e2971d37
jona799t <jonathan.b.skettrup@gmail.com>
2023-10-01 23:16:08 +0200

    Lidt rodet men virker

---
+++ main.py
@@ -0,0 +1,34 @@
+import random_street_view
+from countries import countries
+from threading import Thread
+import json
+
+try:
+    map = open("map.json").readlines()
+    panoIds = [json.loads(location)["panoId"] for location in map] # Dårlig måde at gøre det på
+except FileNotFoundError:
+    open("map.json", "w").write("")
+    panoIds = []
+
+def logger(msg):
+    if msg["code"] == "new_location":
+        if msg["location"]["panoId"] not in panoIds:
+            print("New location:", msg["location"])
+            open("map.json", "a").write(json.dumps(msg["location"]) + "\n")
+    elif msg["code"] == "msg":
+        pass
+        #print(msg["msg"])
+    elif msg["code"] == "done":
+        pass
+        #print(msg["result"])
+
+threads = []
+for _ in range(3): # Amount of workers per country
+    for country in countries:
+        #random_street_view.search(country, 1, 0, 0, logger)
+        thread = Thread(target=random_street_view.search, args=[country, 100, 0, 0, logger])
+        thread.start()
+        threads.append(thread)
+
+for thread in threads:
+    thread.join()
--- random_street_view.py
+++ random_street_view.py
@@ -1,55 +1,9 @@
-import argparse
-import json
 import os
 import random
 import sys
-from urllib.request import urlopen, urlretrieve
 
+import requests
 import shapefile  # pip install pyshp
-
-# Optional, http://stackoverflow.com/a/1557906/724176
-try:
-    import timing
-
-    assert timing  # avoid flake8 warning
-except ImportError:
-    pass
-
-# Google Street View Image API
-# 25,000 image requests per 24 hours
-# See https://developers.google.com/maps/documentation/streetview/
-API_KEY = "INSERT_YOUR_API_KEY_HERE"
-GOOGLE_STREETVIEW_URL = (
-    "http://maps.googleapis.com/maps/api/streetview?sensor=false&"
-    "size=640x640&key=" + API_KEY
-)
-GOOGLE_METADATA_URL = (
-    "https://maps.googleapis.com/maps/api/streetview/metadata?key=" + API_KEY
-)
-
-IMG_PREFIX = "img_"
-IMG_SUFFIX = ".jpg"
-
-parser = argparse.ArgumentParser(
-    description="Get random Street View images from a given country",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument(
-    "-n", "--images-wanted", type=int, default=10, help="Number of images wanted"
-)
-parser.add_argument("country", help="ISO 3166-1 Alpha-3 Country Code")
-parser.add_argument(
-    "-hdg",
-    "--heading",
-    help="Heading in degrees: 0 and 360 north, 90 east, 180 south, 270 west",
-)
-parser.add_argument(
-    "-p",
-    "--pitch",
-    help="Pitch in degrees: 0 is default, 90 straight up, -90 straight down",
-)
-args = parser.parse_args()
-
 
 # Determine if a point is inside a given polygon or not
 # Polygon is a list of (x,y) pairs.
@@ -71,7 +25,6 @@ def point_inside_polygon(x, y, poly):
     return inside
 
 
-print("Loading borders")
 shape_file = "TM_WORLD_BORDERS-0.3.shp"
 if not os.path.exists(shape_file):
     sys.exit(
@@ -82,75 +35,60 @@ if not os.path.exists(shape_file):
 sf = shapefile.Reader(shape_file, encoding="latin1")
 shapes = sf.shapes()
 
-print("Finding country")
-for i, record in enumerate(sf.records()):
-    if record[2] == args.country.upper():
-        print(record[2], record[4])
-        print(shapes[i].bbox)
-        min_lon = shapes[i].bbox[0]
-        min_lat = shapes[i].bbox[1]
-        max_lon = shapes[i].bbox[2]
-        max_lat = shapes[i].bbox[3]
-        borders = shapes[i].points
-        break
+def empty(msg):
+    pass
 
-print("Getting images")
-attempts, country_hits, imagery_hits, imagery_misses = 0, 0, 0, 0
-MAX_URLS = 25000
+def search(country, images_wanted=100, pitch=0, zoom=0, logger=empty): #ISO 3166-1 Alpha-3 Country Code
+    for i, record in enumerate(sf.records()):
+        if record[2] == country.upper():
+            min_lng = shapes[i].bbox[0]
+            min_lat = shapes[i].bbox[1]
+            max_lng = shapes[i].bbox[2]
+            max_lat = shapes[i].bbox[3]
+            borders = shapes[i].points
+            break
 
-if not os.path.exists(args.country):
-    os.makedirs(args.country)
+    attempts, country_hits, imagery_hits, imagery_unofficial, imagery_misses = 0, 0, 0, 0, 0
 
-try:
-    while True:
-        attempts += 1
-        rand_lat = random.uniform(min_lat, max_lat)
-        rand_lon = random.uniform(min_lon, max_lon)
-        # Is (lat,lon) inside borders?
-        if point_inside_polygon(rand_lon, rand_lat, borders):
-            print("  In country")
-            country_hits += 1
-            lat_lon = str(rand_lat) + "," + str(rand_lon)
-            outfile = os.path.join(args.country, IMG_PREFIX + lat_lon + IMG_SUFFIX)
-            streetview_url = GOOGLE_STREETVIEW_URL + "&location=" + lat_lon
-            metadata_url = GOOGLE_METADATA_URL + "&location=" + lat_lon
+    locations = []
+    panoIds = []
+    try:
+        while imagery_hits < images_wanted:
+            logger({"code": "msg", "msg": f"[{country}]: {imagery_hits}/{images_wanted}"})
 
-            with urlopen(metadata_url) as response:
-                body = response.read()
+            attempts += 1
+            rand_lat = random.uniform(min_lat, max_lat)
+            rand_lng = random.uniform(min_lng, max_lng)
+            # Is (lat,lon) inside borders?
+            if point_inside_polygon(rand_lng, rand_lat, borders):
+                country_hits += 1
 
-            metadata_status = json.loads(body)["status"]
-            if metadata_status == "ZERO_RESULTS" or metadata_status == "NOT_FOUND":
-                imagery_misses += 1
-                print(f"  Image not found: {metadata_status}")
-                continue
-            elif metadata_status != "OK":
-                raise RuntimeError(
-                    f"Unexpected response from metadata query: {metadata_status}"
-                )
+                resp = requests.post("https://maps.googleapis.com/$rpc/google.internal.maps.mapsjs.v1.MapsJsInternalService/SingleImageSearch", headers={"Content-Type": "application/json+protobuf"}, data=f'[["apiv3",null,null,null,"US",null,null,null,null,null,[[0]]],[[null,null,{rand_lat},{rand_lng}],{500}],[null,["en","DK"],null,null,null,null,null,null,[2],null,[[[2,1,2],[3,1,2],[10,1,2]]]],[[1,2,3,4,8,6]]]').json()
+                if len(resp) == 1:
+                    imagery_misses += 1
+                    continue
 
-            if args.heading:
-                streetview_url += "&heading=" + args.heading
-            if args.pitch:
-                streetview_url += "&pitch=" + args.pitch
-            try:
-                urlretrieve(streetview_url, outfile)
-            except KeyboardInterrupt:
-                sys.exit("exit")
-
-            if os.path.isfile(outfile):
-                print(lat_lon)
-                print("    ========== Got one! ==========")
                 imagery_hits += 1
-                if imagery_hits == args.images_wanted:
-                    break
-            if country_hits == MAX_URLS:
-                break
-except KeyboardInterrupt:
-    print("Keyboard interrupt")
+                
+                if "Images may be subject to copyright." not in str(resp):
+                    location = {"lat": resp[1][5][0][1][0][2], "lng": resp[1][5][0][1][0][3], "panoId": resp[1][1][1], "heading": round(resp[1][5][0][6][0][1][3]), "pitch": pitch, "zoom": zoom}
 
-print("Attempts:\t", attempts)
-print("Country hits:\t", country_hits)
-print("Imagery misses:\t", imagery_misses)
-print("Imagery hits:\t", imagery_hits)
+                    if location["panoId"] not in panoIds:
+                        logger({"code": "new_location", "location": location})
+                        panoIds.append(location["panoId"])
+                        locations.append(location)
+                    
+                #try:
+                #    location = {"lat": resp[1][5][0][1][0][2], "lng": resp[1][5][0][1][0][3], "panoId": resp[1][1][1], "heading": round(resp[1][5][0][6][0][1][3]), "pitch": pitch, "zoom": zoom}
 
-# End of file
+                #    if location["panoId"] not in panoIds:
+                #        logger({"code": "new_location", "location": location})
+                #        panoIds.append(location["panoId"])
+                #        locations.append(location)
+                #except IndexError:
+                #    imagery_unofficial += 1
+    except KeyboardInterrupt:
+        pass
+
+    logger({"code": "done", "result": locations})
+    return locations, {"attempts": attempts, "country_hits": country_hits, "imagery_misses": imagery_misses, "imagery_hits": imagery_hits, "imagery_unofficial": imagery_unofficial}
